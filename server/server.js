c524ddb25511e54c4390b28e4834961104dbeaa5
Sudip <78307827+sudip57@users.noreply.github.com>
2026-01-03 23:21:09 +0530

    update server configuration

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const timeRangeMiddleware = require("./src/middleware/timeRange");
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend-domain.vercel.app"
+    "https://api-monitoring-system.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
